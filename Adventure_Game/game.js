44e3a1a14bda1bef06f0210b44207cf427d5258f
Unknown <jbloom520@gmail.com>
2019-02-06 10:30:18 -0700

    added even more switches

--- Adventure_Game/game.js
+++ Adventure_Game/game.js
@@ -11,24 +11,33 @@ var pcName = prompt("What is your name?");
 while (!confirm("do you want your name " + pcName)) {
     pcName = prompt("change your name from " + pcName + " to?");
 }
+pcObj = {
+    name: pcName,
+    inventory: {}
+}
+
 if (confirm("Hello player.\nThis is a game made by Jeremy Bloom,\nOptions will be shown as follows:\n\t--This is an example of a option--\nTo choose an option type the first letter of the option.\n Would you like to start the game?")) {
     Game();
 }
+
+//main pathway loop
 function Game() {
     var mainPath = prompt("You wake in a strange room. Padded walls and flooring. You can't seam to to move your arms. They seem to be bound in a strait jacket.\n--Struggle--\n--Look around--\n--Examine strait Jacket--").toLowerCase();
-    switch(mainPath){
+    switch(mainPath[0]){
         
         case "s":
                 alert("You struggle but nothing happens");
                 Game();
                 break;
             case "l":
-                switch(prompt("You look around you find:\n--Red Button--\n--Green Button--\n--Blue Button--\n Which one would you like to press?").toLocaleLowerCase()) {
+                switch(prompt("You look around you find:\n--Red Button--\n--Green Button--\n--Blue Button--\n Which one would you like to press?").toLocaleLowerCase()[0]) {
                     case "r":
+                        alert("Red smoke filles the room and you passout.");
                         Red();
+                        alert("your ran me");
                         break;
                     case "g":
-                        Green();
+                        Green;
                         break;
                     case "b":
                         Blue();
@@ -40,5 +49,11 @@ function Game() {
                 Game();
     }
     }
+function Red(){
+    
+    switch(prompt("")){
+           
+           }
+}
 
 Game();
